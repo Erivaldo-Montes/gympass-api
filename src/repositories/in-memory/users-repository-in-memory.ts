75e673b7e6d46cf433c4483618993723f1d07677
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-06-15 14:15:14 -0300

    a find by id

--- src/repositories/in-memory/users-repository-in-memory.ts
+++ src/repositories/in-memory/users-repository-in-memory.ts
@@ -1,11 +1,12 @@
 import { Prisma, User } from '@prisma/client'
 import { UsersRepository } from '../user-repository'
+import { randomUUID } from 'node:crypto'
 
 export class InMemoryUsersRepository implements UsersRepository {
   public users: User[] = []
   async create(data: Prisma.UserCreateInput): Promise<User> {
     const user = {
-      id: 'user-1',
+      id: randomUUID(),
       name: data.name,
       email: data.email,
       password_hash: data.password_hash,
@@ -19,6 +20,16 @@ export class InMemoryUsersRepository implements UsersRepository {
 
   async findByEmail(email: string): Promise<User | null> {
     const user = this.users.find((user) => user.email === email)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+
+  async findById(id: string): Promise<User | null> {
+    const user = this.users.find((user) => user.id === id)
 
     if (!user) {
       return null
