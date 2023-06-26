a42c5d839ed3366a4e28a25e933f33c7f60d88ad
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-06-25 21:01:06 -0300

    core: distance functions

--- src/utils/get-distance-between-coordenate.ts
+++ src/utils/get-distance-between-coordenate.ts
@@ -3,35 +3,55 @@ export interface Coordinates {
   latitude: number
 }
 
+function toRadians(degrees: number) {
+  return (degrees * Math.PI) / 180
+}
+
 export function getDistanceBetweenCoodintes(
   from: Coordinates,
   to: Coordinates,
 ) {
-  if (from.latitude === to.latitude && from.longitude === to.longitude) {
-    return 0
-  }
+  const R = 6371
 
-  const fromRadian = (Math.PI * from.latitude) / 180
-  const toRadian = (Math.PI * to.latitude) / 180
+  const diffLatitute = toRadians(from.latitude - to.latitude)
+  const diffLongitude = toRadians(from.longitude - to.longitude)
 
-  const theta = from.longitude - to.longitude
-  const radTheta = (Math.PI * theta) / 180
+  const a =
+    Math.sin(diffLatitute / 2) ** 2 +
+    Math.cos(toRadians(from.latitude)) *
+      Math.cos(toRadians(to.latitude)) *
+      Math.sin(diffLongitude / 2) ** 2
 
-  let dist =
-    Math.sin(fromRadian) *
-    Math.sin(toRadian) *
-    Math.cos(fromRadian) *
-    Math.cos(toRadian) *
-    Math.cos(radTheta)
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
 
-  if (dist > 1) {
-    dist = 1
-  }
+  const distance = R * c
+  return distance
 
-  dist = Math.acos(dist)
-  dist = (dist * 180) / Math.PI
-  dist = dist * 60 * 1.1515
-  dist = dist * 1.609344
+  // if (from.latitude === to.latitude && from.longitude === to.longitude) {
+  //   return 0
+  // }
 
-  return dist
+  // const fromRadian = (Math.PI * from.latitude) / 180
+  // const toRadian = (Math.PI * to.latitude) / 180
+
+  // const theta = from.longitude - to.longitude
+  // const radTheta = (Math.PI * theta) / 180
+
+  // let dist =
+  //   Math.sin(fromRadian) *
+  //   Math.sin(toRadian) *
+  //   Math.cos(fromRadian) *
+  //   Math.cos(toRadian) *
+  //   Math.cos(radTheta)
+
+  // if (dist > 1) {
+  //   dist = 1
+  // }
+
+  // dist = Math.acos(dist)
+  // dist = (dist * 180) / Math.PI
+  // dist = dist * 60 * 1.1515
+  // dist = dist * 1.609344
+
+  // return dist?
 }
