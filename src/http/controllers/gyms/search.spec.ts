4033d4f4b83a1fd2c7707c8f1f51ff5a445f5f3f
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-06-28 21:53:29 -0300

    core: search gyms test e2e

---
+++ src/http/controllers/gyms/search.spec.ts
@@ -0,0 +1,53 @@
+import request from 'supertest'
+import { app } from '@/app'
+
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { CreateAndAuthenticateUser } from '@/utils/test/create-and-authenticate'
+
+describe('Search gym', () => {
+  beforeAll(() => {
+    app.ready()
+  })
+
+  afterAll(() => {
+    app.close()
+  })
+
+  it('Should be able search a gym', async () => {
+    const { token } = await CreateAndAuthenticateUser(app)
+
+    await request(app.server)
+      .post('/gym')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Jhon Doe Academy',
+        description: 'some decription',
+        phone: '12345',
+        latitude: 7.7517794,
+        longitude: -35.6053779,
+      })
+
+    await request(app.server)
+      .post('/gym')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Bob Academy',
+        description: 'some decription',
+        phone: '123458',
+        latitude: 7.751779,
+        longitude: -35.6053779,
+      })
+
+    const response = await request(app.server)
+      .get('/gym/search')
+      .set('Authorization', `Bearer ${token}`)
+      .query({ q: 'Bob' })
+      .send()
+
+    expect(response.status).toEqual(200)
+    expect(response.body.gyms).toHaveLength(1)
+    expect(response.body.gyms).toEqual([
+      expect.objectContaining({ title: 'Bob Academy' }),
+    ])
+  })
+})
